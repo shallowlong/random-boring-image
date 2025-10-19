7266c60fe57420e50d112a4513eec80a59264afc
shallowlong <shallowlong@gmail.com>
2025-10-20 01:26:24 +0800

    ..

--- api/app.js
+++ api/app.js
@@ -20,7 +20,11 @@ app.get('/scrape', async (req, res) => {
 		const { data } = await axios.get(url, {
 			headers: {
 				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
-				"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
+				"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+				"Accept-Encoding": "gzip, deflate, br",
+				"Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
+				"Connection": "keep-alive",
+				"Upgrade-Insecure-Requests": "1"
 			}
 		});
 		const $ = cheerio.load(data);
