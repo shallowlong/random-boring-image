071c52237bd0a7b087120f246a4c82c0e87764fb
shallowlong <shallowlong@gmail.com>
2025-10-20 00:33:00 +0800

    try to fix server block problem

--- api/app.js
+++ api/app.js
@@ -17,7 +17,12 @@ app.get('/', function (req, res) {
 
 app.get('/scrape', async (req, res) => {
 	try {
-		const { data } = await axios.get(url);
+		const { data } = await axios.get(url, {
+			headers: {
+				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
+				"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
+			}
+		});
 		const $ = cheerio.load(data);
 
 		const imgUrls = [];
